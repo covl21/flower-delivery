7cf9f1307d132b4b46e9df45c23e16b7f47acc6c
covl21 <volodymyr11reus@gmail.com>
2025-09-12 11:54:34 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,17 +7,17 @@ export default function Header() {
     <header className="header">
       <div className="header-left">
         <Link to="/" className="header-section">
-          <img src="/icons/shop.png" alt="shop" />
+          <img src="/public/icons/shop.png" alt="shop" />
           <span>Shop</span>
         </Link>
         <Link to="/cart" className="header-section">
-          <img src="/icons/cart.png" alt="cart" />
+          <img src="/public/icons/cart.png" alt="cart" />
           <span>Shoping Cart</span>
         </Link>
       </div>
 
       <h1>
-        Flower-delivery <img src="/icons/flower.png" alt="flower" />
+        Flower-delivery <img src="/public/icons/flower.png" alt="flower" />
       </h1>
     </header>
   );
