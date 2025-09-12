2608ffe69a6dc7796251c0bdefab99993a714c48
covl21 <volodymyr11reus@gmail.com>
2025-09-12 10:06:51 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Статичні картинки з папки public/images доступні за /images/...
 app.use("/images", express.static(path.join(__dirname, "public", "images")));
 
 // API
